9e45347fb6bc6935bd773ddc359c40d1fe22c8b7
Rob Loach <robloach@gmail.com>
2015-07-07 17:46:01 -0400

    Ignore partials, and allow includes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var jstransformer = require('jstransformer')
 var listOfJsTransformers = require('list-of-jstransformers')
 var transformers = {}
+var path = require('path')
+var extend = require('extend')
 
 /**
  * Get the transformer from the given name.
@@ -28,29 +30,37 @@ module.exports = function (opts) {
   return function (files, metalsmith, done) {
     // Loop through every file.
     for (var file in files) {
-      // Get all the transforming extensions.
-      var extensions = file.split('.')
+      // Ignore all partials.
+      if (path.basename(file).charAt(0) !== '_') {
+        // Get all the transforming extensions.
+        var extensions = file.split('.')
+        for (var i = extensions.length - 1; i > 0; i--) {
+          // Retrieve the transformer.
+          var transformer = getTransformer(extensions[i])
+          if (transformer) {
+            // Process the content of the file with the transformer.
+            var input = files[file].contents.toString()
 
-      for (var i = extensions.length - 1; i > 0; i--) {
-        // Retrieve the transformer.
-        var transformer = getTransformer(extensions[i])
-        if (transformer) {
-          // Process the content of the file with the transformer.
-          var input = files[file].contents.toString()
-          // TODO: Figure out what to do for options.
-          var output = transformer.render(input, files[file], files[file])
-          files[file].contents = new Buffer(output.body)
+            // Construct the options.
+            var options = extend({}, files[file], {
+              filename: metalsmith.source() + '/' + file
+            })
 
-          // Rename the file, removing the extension.
-          var data = files[file]
-          delete files[file]
-          var filepath = extensions
-          filepath.pop()
-          file = filepath.join('.')
-          files[file] = data
-        } else {
-          // Skip the rest of the file transformations.
-          break
+            // TODO: Figure out what to do for options.
+            var output = transformer.render(input, options, options)
+            files[file].contents = new Buffer(output.body)
+
+            // Rename the file, removing the extension.
+            var data = files[file]
+            delete files[file]
+            var filepath = extensions
+            filepath.pop()
+            file = filepath.join('.')
+            files[file] = data
+          } else {
+            // Skip the rest of the file transformations.
+            break
+          }
         }
       }
     }
